08b95ec51a4464e66212f4c49efd1d9ae231b744
Yuuki <982498772@qq.com>
2023-12-05 15:59:10 +0800

    fix proxyPattern logic

--- ProxyPattern/source.go
+++ ProxyPattern/source.go
@@ -1,1 +1,47 @@
 package ProxyPattern
+
+import "fmt"
+
+type IGiveGift interface {
+	GiveDolls()
+	GiveFollowers()
+	GiveChocolate()
+}
+
+type SchoolGirl struct {
+	name string
+}
+
+type PurSuit struct {
+	SchoolGirl
+	// name string
+}
+
+type Proxy struct {
+	PurSuit
+	// name string
+}
+
+func (p *PurSuit) GiveDolls() {
+	fmt.Println(p.SchoolGirl.name + "送你洋娃娃")
+}
+
+func (p *PurSuit) GiveFollowers() {
+	fmt.Println(p.SchoolGirl.name + "送你花")
+}
+
+func (p *PurSuit) GiveChocolate() {
+	fmt.Println(p.SchoolGirl.name + "送你巧克力")
+}
+
+func (p *Proxy) GiveDolls() {
+	p.PurSuit.GiveDolls()
+}
+
+func (p *Proxy) GiveFollowers() {
+	p.PurSuit.GiveFollowers()
+}
+
+func (p *Proxy) GiveChocolate() {
+	p.PurSuit.GiveChocolate()
+}
--- ProxyPattern/source_test.go
+++ ProxyPattern/source_test.go
@@ -1,1 +1,12 @@
 package ProxyPattern
+
+import "testing"
+
+func TestProxyPattern(t *testing.T) {
+	girl := SchoolGirl{name: "jiaojiao"}
+	pursuit := PurSuit{girl}
+	proxy := Proxy{pursuit}
+	proxy.GiveDolls()
+	proxy.GiveFollowers()
+	proxy.GiveChocolate()
+}
